6bf1aaa64bdbe48eca35dfde964644bed1bf4b57
Delthin <1059661071@qq.com>
2024-03-12 11:59:53 +0800

    v0.3轮船还有问题，一直0 3

--- src/com/huawei/codecraft/AssignTargetBoat.java
+++ src/com/huawei/codecraft/AssignTargetBoat.java
@@ -16,11 +16,16 @@ public interface AssignTargetBoat {
             for(Boat boat:boats){
                 if(boat.getState()==0)continue;;//此时船只处于移动中，相当于消失
                 if(boat.getState()==1){
+                    //初始化船只状态为state=1，TargetBerthId=-1
+                    if(boat.getTargetBerthId() == -1){
+                        boat.setState(2);
+                    }
                     //此时船只在港口等待,(是否是只要船只已满，就变成状态0了？无需在此处讨论是否已满？)
                     if(boat.getVacancy()==0){
-                        boat.setState(0);//如果此时船只剩余空间为零，将状态设为0
+                        boat.setAction(2);//如果此时船只剩余空间为零，将状态设为0
                     }else{//如果还有空间
                         //todo:进行装货物操作
+                        //先把下面的移过来，后面再讨论这种state属于哪个
 
                     }
 
@@ -30,6 +35,7 @@ public interface AssignTargetBoat {
                     Berth bestBerth=findBestBerth(berths,boats);
                     if(bestBerth!=null){
                         boat.setTargetBerthId(bestBerth.getId());
+                        boat.setAction(1);
                     }
 
                 }
@@ -39,7 +45,7 @@ public interface AssignTargetBoat {
             //找到最优泊位：前提为空，取效率最大的
             Berth bestBerth = null;
             boolean isEmpty=true;
-            ArrayList<Berth> emptyBerthList =null;
+            ArrayList<Berth> emptyBerthList = new ArrayList<>();
             int index=0;
             for(Berth berth:berths){
                 for(Boat boat:boats){
